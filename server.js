d019e97e32f9768697ea596919531e1cb2213de1
sanmcc <120269286+sanmcc@users.noreply.github.com>
2023-06-06 21:52:53 -0600

    ugh

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const path = require('path');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 //hndlbrs w helpers
 const hbs = exphbs.create({ helpers });
 
@@ -39,7 +40,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
